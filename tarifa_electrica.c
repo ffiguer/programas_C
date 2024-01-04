5df1c95720740cb6201bb7b8461a7ce5a9326b2e
ffiguer <152465676+ffiguer@users.noreply.github.com>
2024-01-03 22:55:49 -0500

    Update tarifa_electrica.c
    modificación con comentarios

--- tarifa_electrica.c
+++ tarifa_electrica.c
@@ -1,14 +1,14 @@
 # include <stdio.h>
 int main()
 {
-	float tarifa1=1.2, tarifa2=1.0, tarifa3=0.9, gasto;
-	printf("\n\n\t introduzca el gasto en KWh de este mes marcado por el contador:");
-	scanf("%f",& gasto);
-	if(gasto < 1000)
-	printf("\n\n\t la tarifa a cobrar este mes es de: %.2f",tarifa1);
-	if(gasto >= 1000 && gasto <= 1850)
-	printf("\n\n\t la tarifa a cobrar este mes es de: %.2f",tarifa2);
-	if(gasto > 1850)
-	printf("\n\n\t la tarifa a cobrar este mes es de: %.2f",tarifa3);
-	return 0;
-}
+	float tarifa1=1.2, tarifa2=1.0, tarifa3=0.9, gasto;   //definicion de variables
+	printf("\n\n\t introduzca el gasto en KWh de este mes marcado por el contador:"); //introduzca el valor marcado por el contador
+	scanf("%f",& gasto); //captura del dato
+	if(gasto < 1000) //evaluación de la primera condicion
+	printf("\n\n\t la tarifa a cobrar este mes es de: %.2f",tarifa1); //imprime teniendo en cuenta la primera condición
+	if(gasto >= 1000 && gasto <= 1850) //evaluación de la segunda condicion
+	printf("\n\n\t la tarifa a cobrar este mes es de: %.2f",tarifa2);  //imprime teniendo en cuenta la segunda condición
+	if(gasto > 1850)  //evaluacion de la tercera condición
+	printf("\n\n\t la tarifa a cobrar este mes es de: %.2f",tarifa3);  //imprime teniendo en cuenta la tercera condición
+	return 0; //retorne 0
+}       //fin del programa
